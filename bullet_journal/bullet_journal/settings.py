f9a64f64c6a9a4936226eeae228c44523da36b5c
Shainal Chetty <shainalchetty89@gmail.com>
2019-03-14 08:12:33 +0200

    Changed setting to connect to postgres db

--- bullet_journal/bullet_journal/settings.py
+++ bullet_journal/bullet_journal/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'bullet_journal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bulletjournaldb', 
+        'USER': 'shainal',
+        'PASSWORD': 'postgreslocalpwd',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
