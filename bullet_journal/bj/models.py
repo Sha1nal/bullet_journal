9b6eaad0082d1761e68a00651a65cb71fcceb680
Shainal Chetty <shainalchetty89@gmail.com>
2019-03-18 11:24:31 +0200

    Created model for Notes, made migrations
        -Added app to INSTALLED_APPS

--- bullet_journal/bj/models.py
+++ bullet_journal/bj/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Notes(models.Model):
+    note_title = models.CharField(max_length=140)
+    note_text = models.TextField()
+    date_created = models.DateField(auto_now=True)
+    expiry_date = models.DateField()
+    url = models.URLField()
+    note_type = models.CharField(max_length=1,choices = (
+                                                    ('N', 'Note'),
+                                                    ('E', 'Event'),
+                                                    ('T', 'Task'),
+                                                )
+                                                )
+
+    def __str__(self):
+        return self.note_title
