0dbef46128dff65d6b0171799e1b5f9ef9980531
Shainal Chetty <shainalchetty89@gmail.com>
2019-03-21 21:39:19 +0200

    Created Tags,Notes_Notes and Tag_Notes models
        -Made migrations

--- bullet_journal/bj/models.py
+++ bullet_journal/bj/models.py
@@ -15,3 +15,26 @@ class Notes(models.Model):
 
     def __str__(self):
         return self.note_title
+
+
+class Tasks(models.Model):
+    task_text = models.TextField()
+    is_complete = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.task_text
+
+
+class Notes_Notes(models.Model):
+    note_text = models.TextField()
+    date_created = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.note_text
+
+class Tags(models.Model):
+    tag_text = models.Charfield(max_length=20)
+    note_id = models.ForeignKey(Notes_Notes, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.tag_text
