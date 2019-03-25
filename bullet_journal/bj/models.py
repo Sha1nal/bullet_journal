af5d98e00b51b827d8c97b1acb46ff08cd347aa8
Shainal Chetty <shainalchetty89@gmail.com>
2019-03-25 18:22:06 +0200

    Removed Notes Table

--- bullet_journal/bj/models.py
+++ bullet_journal/bj/models.py
@@ -1,22 +1,5 @@
 from django.db import models
 
-class Notes(models.Model):
-    note_title = models.CharField(max_length=140)
-    note_text = models.TextField()
-    date_created = models.DateField(auto_now=True)
-    expiry_date = models.DateField()
-    url = models.URLField()
-    note_type = models.CharField(max_length=1,choices = (
-                                                    ('N', 'Note'),
-                                                    ('E', 'Event'),
-                                                    ('T', 'Task'),
-                                                )
-                                                )
-
-    def __str__(self):
-        return self.note_title
-
-
 class Tasks(models.Model):
     task_text = models.TextField()
     is_complete = models.BooleanField(default=False)
